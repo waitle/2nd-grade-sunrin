d8635faa7e7c057771fcf2202266338a8dd9ef61
Kim teak seo <rlaxortj00@gmail.com>
2017-03-08 11:28:58 +0900

    move

---
+++ game programing/3.8/ConsoleApplication1/useing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 20;
+	cout << "web operating" << endl;
+	cout << "web" << "operating" << endl;
+	cout << num << ' ' << 'a';
+	cout << ' ' << "3.14<<endl";
+}
