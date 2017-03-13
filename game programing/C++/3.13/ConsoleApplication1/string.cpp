b553fa58cea8cfdbc597edfb565a0b7f56955ce6
Kim teak seo <rlaxortj00@gmail.com>
2017-03-13 14:21:24 +0900

    3.13

---
+++ game programing/C++/3.13/ConsoleApplication1/string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s1 = "gud";
+	string s2 = "morning";
+	string s3 = s1 + " " +s2 +"!\n";
+	cout << s3;
+	return 0;
+}
