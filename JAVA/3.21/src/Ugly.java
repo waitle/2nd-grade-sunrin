a31feb04763e4f15ea3c9881f31a907da7eae65f
Kim teak seo <rlaxortj00@gmail.com>
2017-03-21 12:21:54 +0900

    3.21

---
+++ JAVA/3.21/src/Ugly.java
@@ -0,0 +1,10 @@
+public class Ugly {
+	public static void main( String[]       args) {
+		System.out .println(  "Hello w"+ "orld");
+byte a= 1;
+byte b =2;
+int c= a + b;
+
+System.out.println(a);
+}
+	}
---
+++ JAVA/3.21/src/exam.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class exam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JOptionPane.showMessageDialog(null, "hello " + JOptionPane.showInputDialog("what is your name?"));
+		System.exit(0);
+	}
+
+}
