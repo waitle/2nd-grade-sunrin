d5735783de8654e4bae6c8fda49453457e31dfec
Kim teak seo <rlaxortj00@gmail.com>
2017-04-05 20:08:45 +0900

    .

--- programing/NC/NC/소스.c
+++ programing/NC/NC/소스.c
@@ -1,33 +1,34 @@
 #include<stdio.h>
 
 int main() {
+   int n, i, j, x, y;
+   int a[20][20] = {0, };
+   for (i = 1; i <= 19; i++) {
+      for (j = 1; j <= 19; j++) {
+         scanf("%d", &a[i][j]);
+      }
+   }
+   scanf("%d", &n);
 
-	int input;
-	int num[20][20] = { 0, };
-	int i, j;
-	int go = 1;
-
-
-	printf("입력하세요\n");
-	scanf_s("%d", &input);
-
-	for (i = 0; i < input; i++) {
-		for (j = 0; j < input; j++) {
-			num[j][i] = go++;
-		}
-	}
-	for (j = 0; j < input; j++) {
-		if (j % 2 == 0) {
-			for (i = 0; i < input; i++) {
-				printf("%4d", num[i][j]);
-			}
-		}
-		else if (j % 2 == 1) {
-			for (i = input - 1; i >= 0; i--) {
-				printf("%4d", num[i][j]);
-			}
-			printf("\n");
-		}
-	}
-	return 0;
-}
+   for (i = 1; i <= n; i++)
+   {
+      scanf("%d %d", &x, &y);
+      for (j = 1; j <= 19; j++)
+      {
+         if (a[x][j] == 0) { a[x][j] = 1; }
+         else { a[x][j] = 0; }
+      }
+      for (j = 1; j <= 19; j++)
+      {
+         if (a[j][y] == 0) { a[j][y] = 1; }
+         else { a[j][y] = 0; }
+      }
+   }
+   for (i = 1; i <= 19; i++) {
+      for (j = 1; j <= 19; j++) {
+         printf("%d ", a[i][j]);
+      }
+      printf("\n");
+   }
+   return 0;
+   }
